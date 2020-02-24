85447a3a5e2930bc2a374d7b2ef16d62968cda32
ffdd270 <ffdd270@gmail.com>
2020-02-24 23:54:16 +0900

    Add Header Design Chat Box.

--- src/components/ui/chat_box.js
+++ src/components/ui/chat_box.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {makeStyles, withStyles} from '@material-ui/core/styles';
-import {Card, Paper, TextField} from "@material-ui/core";
+import {Card, CardHeader, Paper, TextField, Avatar, CardContent} from "@material-ui/core";
+import { red } from '@material-ui/core/colors';
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import {Socket} from "../system/socket";
@@ -12,22 +13,57 @@ const styles = (theme) =>(
             padding: 0,
             'box-sizing': 'border-box'
         },
+        paper: {
+
+        },
+        avatar: {
+            backgroundColor: red[500],
+            width: 40,
+            height: 40,
+        },
+        user_name: {
+            fontSize: "1.2rem",
+            position: 'relative',
+            bottom: 10,
+        },
+        content_pos: {
+            position: 'relative',
+            left: 56,
+            top: -50,
+        },
+        text: {
+            fontSize: "1.0rem"
+        }
     }
 );
 
 
 class ChatBox extends React.Component
 {
-
     render() {
+        const {classes} = this.props;
+        let name = this.props.name === undefined ? '하루가카' : this.props.name;
 
         return (
             <div>
-                {this.props.text}
+                <Card elevation={0}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label={"recipe"} className={classes.avatar}>
+                                {name[0]}
+                            </Avatar>}
+                        title={ <div className={classes.user_name}>{name}</div> }
+                    />
+                    <CardContent className={classes.content_pos}>
+                        <Typography variant={"h3"} className={classes.text}>
+                            {this.props.text}
+                        </Typography>
+                    </CardContent>
+                </Card>
             </div>
         )
     }
 
 }
 
-export default ChatBox;
+export default withStyles(styles)( ChatBox );
