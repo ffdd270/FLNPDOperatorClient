289e1d91922f28651b7a1d8dcabac77e712c959b
ffdd270 <ffdd270@gmail.com>
2020-03-08 23:59:27 +0900

    Add Style And Textbox.

--- src/components/routes/create_battle.js
+++ src/components/routes/create_battle.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {makeStyles, withStyles} from '@material-ui/core/styles';
-import CharacterCardForm from '../ui/character_card_form'
 import socketio from "socket.io-client";
 import {Socket} from "../system/socket";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import {post_query} from "../system/util";
+import DialogContent from "@material-ui/core/DialogContent";
 
 const styles = (theme) => (
     {
@@ -13,8 +15,18 @@ const styles = (theme) => (
 
 class CreateBattlePage extends  React.Component
 {
+    state ={
+        name: ""
+    };
+
     constructor(props) {
         super(props);
+        this.handleValueChange = this.handleValueChange.bind( this );
+    }
+
+    handleValueChange( event )
+    {
+
     }
 
     query( battle_name )
@@ -23,8 +35,16 @@ class CreateBattlePage extends  React.Component
             battle_id: battle_name
         });
     }
+
+
     render() {
+        return(
+            <div>
+                <TextField label="이름" type="text" name="name" value={this.state.name} onChange={this.handleValueChange} />
 
-
+            </div>
+        )
     }
 }
+
+export default withStyles(styles)(CreateBattlePage)
